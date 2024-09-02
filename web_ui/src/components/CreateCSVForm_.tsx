e285469b087d2e88f1b9288db7b929dbda8fac94
LightGrey <92320503+LeraPolovinkina@users.noreply.github.com>
2024-09-02 14:52:01 +0600

    NERForm was made

--- web_ui/src/components/CreateCSVForm_.tsx
+++ web_ui/src/components/CreateCSVForm_.tsx
@@ -1,38 +1,109 @@
-import {useForm} from "react-hook-form";
-import {useState} from "react";
-import {Strategy} from "../models/CreateCsvOptions.ts";
+// import {useForm} from "react-hook-form";
+// import {useState} from "react";
+// import {Strategy} from "../models/CreateCsvOptions.ts";
+//
+//
+// const CreateCSVForm = () => {
+//   const gptOptionsForm = useForm<IGPTForm>({
+//     // defaultValues: {...}
+//   })
+//   const nerOptionsForm = useForm<INERForm>({
+//     // defaultValues: {...}
+//   })
+//
+//   const [strategy, setStrategy] = useState<Strategy>("ner")
+//
+//   let form= <NERForm />
+//
+//   switch (strategy) {
+//     case "ner":
+//       form = <NERForm />
+//       break
+//     case "gpt":
+//       form = <GPTForm />
+//       break
+//   }
+// }
+//
+//   return (
+//     // https://mui.com/material-ui/react-select/
+//     // <Select>...</Select>
+//
+//     // https://www.react-hook-form.com/advanced-usage/#FormProviderPerformance
+//     // https://habr.com/ru/articles/746806/
+//     <FormProvider {...methods}>
+//       { form }
+//     </FormProvider>
+//   )
+// }
+
+
+
+import React from 'react';
+import { useForm, FormProvider } from 'react-hook-form';
+import { useState } from 'react';
+import NERForm from './NERForm';
+import GPTForm from './GPTForm';
+import { INERForm, IGPTForm } from '../models/types';
 
 
 const CreateCSVForm = () => {
-  const gptOptionsForm = useForm<IGPTForm>({
-    // defaultValues: {...}
-  })
-  const nerOptionsForm = useForm<INERForm>({
-    // defaultValues: {...}
-  })
+    const gptOptionsForm = useForm<IGPTForm>({
+        defaultValues: {
+            companies: "",
+            sites: "",
+            search_query_template: "{company} AND {sites}",
+            max_lead_count: 10
+        },
+    });
+    const nerOptionsForm = useForm<INERForm>({
+        defaultValues: {
+            companies: "",
+            sites: "",
+            positions: "",
+            search_query_template: "{company} AND {positions} AND {site}",
+            max_lead_count: 10
+        },
+    });
 
-  const [strategy, setStrategy] = useState<Strategy>("ner")
+    const [strategy, setStrategy] = useState<Strategy>("ner");
 
-  let form= <NERForm />
+    const handleStrategyChange = (event: React.ChangeEvent<{ value: unknown }>) => {
+        setStrategy(event.target.value as Strategy);
+    };
 
-  switch (strategy) {
-    case "ner":
-      form = <NERForm />
-      break
-    case "gpt":
-      form = <GPTForm />
-      break
-  }
-}
+    const methods = strategy === "ner" ? nerOptionsForm : gptOptionsForm;
 
-  return (
-    // https://mui.com/material-ui/react-select/
-    // <Select>...</Select>
+    let form = <NERForm />;
+      switch (strategy) {
+        case "ner":
+          form = <NERForm />
+          break
+        case "gpt":
+          form = <GPTForm />
+          break
+          default:
+              form = <NERForm />;
+                break;
+      }
 
-    // https://www.react-hook-form.com/advanced-usage/#FormProviderPerformance
-    // https://habr.com/ru/articles/746806/
-    <FormProvider {...methods}>
-      { form }
-    </FormProvider>
-  )
-}
+
+    return (
+        <FormProvider {...methods}>
+            <form onSubmit={methods.handleSubmit(data => console.log(data))}>
+                <Stack spacing={3}>
+                    <Select
+                        value={strategy}
+                        onChange={handleStrategyChange}
+                        variant="outlined"
+                        displayEmpty
+                    >
+                        <MenuItem value="ner">NER</MenuItem>
+                        <MenuItem value="gpt">GPT-4</MenuItem>
+                    </Select>
+                    {form}
+                </Stack>
+            </form>
+        </FormProvider>
+    );
+};
--- web_ui/src/components/NERForm.tsx
+++ web_ui/src/components/NERForm.tsx
@@ -1,3 +1,23 @@
+// interface INERForm {
+//     companies: string;
+//     sites: string;
+//     positions: string;
+//     search_query_template: string;
+//     max_lead_count: number;
+// }
+//
+// const NERForm = () => {
+//     // const {} = useFormContext();
+//
+//     // Возвращаем нужные для NER контроллеры
+//     // return ()
+// }
+
+
+import React from 'react';
+import { useFormContext, Controller } from 'react-hook-form';
+import { Stack, TextField } from '@mui/material';
+
 interface INERForm {
     companies: string;
     sites: string;
@@ -6,9 +26,74 @@ interface INERForm {
     max_lead_count: number;
 }
 
-const NERForm = () => {
-    // const {} = useFormContext();
+const NERForm: React.FC = () => {
+    const { control } = useFormContext<INERForm>();
 
-    // Возвращаем нужные для NER контроллеры
-    // return ()
-}
+    return (
+        <Stack spacing={2}>
+            <Controller
+                name="companies"
+                control={control}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Компании"
+                        placeholder="Введите названия компаний"
+                        multiline
+                        rows={4}
+                    />
+                )}
+            />
+            <Controller
+                name="sites"
+                control={control}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Сайты"
+                        placeholder="Введите URL сайтов"
+                        multiline
+                        rows={4}
+                    />
+                )}
+            />
+            <Controller
+                name="positions"
+                control={control}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Должности"
+                        placeholder="Введите должности"
+                        multiline
+                        rows={4}
+                    />
+                )}
+            />
+            <Controller
+                name="search_query_template"
+                control={control}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Шаблон поискового запроса"
+                        placeholder="Введите шаблон"
+                    />
+                )}
+            />
+            <Controller
+                name="max_lead_count"
+                control={control}
+                render={({ field }) => (
+                    <TextField
+                        {...field}
+                        label="Максимальное количество лидов"
+                        type="number"
+                    />
+                )}
+            />
+        </Stack>
+    );
+};
+
+export default NERForm;
